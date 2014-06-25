e3634d00a1de64b69d3d1bfcc31ea661a9ab2777
Satheeshwaran J <satheeshwaran.j@cognizant.com>
2014-06-25 19:42:45 +0530

    Initial Commit

---
+++ NotificationsforiOS8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationsforiOS8
+//
+//  Created by Satheeshwaran on 6/25/14.
+//  Copyright (c) 2014 Cognizant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
